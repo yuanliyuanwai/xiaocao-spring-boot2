04686f048cd0c71b2d9f79674c61bf7b5c4c30ee
yuanliyuanwai <wanzhengchong@qq.com>
2019-07-22 22:07:24 +0800

    spring boot打成war包

---
+++ spring-boot-package-war/src/test/java/in/xiaocao/ApplicationTests.java
@@ -0,0 +1,18 @@
+package in.xiaocao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import in.xiaocao.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
