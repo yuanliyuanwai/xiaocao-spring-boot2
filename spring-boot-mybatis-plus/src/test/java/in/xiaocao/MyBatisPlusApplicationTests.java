94c59320aed5a718f6249bceca1d7ba72eda070e
yuanliyuanwai <wanzhengchong@qq.com>
2019-10-13 14:27:47 +0800

    mybatis-plus

---
+++ spring-boot-mybatis-plus/src/test/java/in/xiaocao/MyBatisPlusApplicationTests.java
@@ -0,0 +1,18 @@
+package in.xiaocao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import in.xiaocao.MyBatisPlusApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyBatisPlusApplication.class)
+public class MyBatisPlusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
