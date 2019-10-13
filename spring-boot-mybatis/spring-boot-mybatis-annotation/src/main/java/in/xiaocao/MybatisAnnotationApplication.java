58395ea5bb6ab449bfce0603cbf4ebaa06ba7c8f
yuanliyuanwai <wanzhengchong@qq.com>
2019-10-13 11:04:26 +0800

    spring-boot-mybatis-annotation

---
+++ spring-boot-mybatis/spring-boot-mybatis-annotation/src/main/java/in/xiaocao/MybatisAnnotationApplication.java
@@ -0,0 +1,14 @@
+package in.xiaocao;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("in.xiaocao.mapper")
+public class MybatisAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisAnnotationApplication.class, args);
+	}
+}
