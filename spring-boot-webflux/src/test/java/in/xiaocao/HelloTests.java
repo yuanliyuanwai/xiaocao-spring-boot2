9b5c803359ce32ac0134d795ef4f1497dff3d38c
yuanliyuanwai <wanzhengchong@qq.com>
2020-11-08 20:26:24 +0800

    响应式编程

---
+++ spring-boot-webflux/src/test/java/in/xiaocao/HelloTests.java
@@ -0,0 +1,22 @@
+package in.xiaocao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import in.xiaocao.web.HelloController;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest(controllers = HelloController.class)
+public class HelloTests {
+    @Autowired
+    WebTestClient client;
+
+    @Test
+    public void getHello() {
+        client.get().uri("/hello").exchange().expectStatus().isOk();
+    }
+}
