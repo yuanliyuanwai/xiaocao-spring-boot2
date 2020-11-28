5185c8f8752a33eef6ddd4f6dd72f48903b88e30
yuanliyuanwai <wanzhengchong@qq.com>
2020-11-28 22:02:15 +0800

    spring-boot-multi-mongodb

---
+++ spring-boot-mongodb/spring-boot-multi-mongodb/src/test/java/in/xiaocao/repository/MuliDatabaseTest.java
@@ -0,0 +1,46 @@
+package in.xiaocao.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import in.xiaocao.model.User;
+import in.xiaocao.repository.primary.PrimaryRepository;
+import in.xiaocao.repository.secondary.SecondaryRepository;
+
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MuliDatabaseTest {
+
+    @Autowired
+    private PrimaryRepository primaryRepository;
+
+    @Autowired
+    private SecondaryRepository secondaryRepository;
+
+    @Test
+    public void TestSave() {
+        System.out.println("************************************************************");
+        System.out.println("测试开始");
+        System.out.println("************************************************************");
+        this.primaryRepository.save(new User("小张", "123456"));
+        this.secondaryRepository.save(new User("小王", "654321"));
+        List<User> primaries = this.primaryRepository.findAll();
+        for (User primary : primaries) {
+            System.out.println(primary.toString());
+        }
+        List<User> secondaries = this.secondaryRepository.findAll();
+        for (User secondary : secondaries) {
+            System.out.println(secondary.toString());
+        }
+        System.out.println("************************************************************");
+        System.out.println("测试完成");
+        System.out.println("************************************************************");
+    }
+
+}
