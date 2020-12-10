224031db4398c472486ce8c6abc8684f5316ecab
yuanliyuanwai <wanzhengchong@qq.com>
2020-12-10 14:46:30 +0800

    spring-boot-hello

--- spring-boot-hello/src/main/java/in/xiaocao/config/RateLimitConfig.java
+++ spring-boot-hello/src/main/java/in/xiaocao/config/RateLimitConfig.java
@@ -1,6 +1,9 @@
 package in.xiaocao.config;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
@@ -13,13 +16,15 @@ import in.xiaocao.dto.UrlPolicy;
 @ConfigurationProperties(prefix = "xiaocao.rate.limit")
 public class RateLimitConfig {
 	
+	private Policy defaultPolicy; // 默认限流策略
+	
 	private Policy ipPolicy; // ip限流策略
 	
-	private List<IPPolicy> perIpPolicies; // 每个ip限流策略
+	private Map<String, IPPolicy> perIpPolicyMap = new HashMap<String, IPPolicy>(); // 每个ip限流策略
 	
 	private Policy urlPolicy; // url限流策略
 	
-	private List<UrlPolicy> perUrlPolicies; // 每个url限流策略
+	private Map<String, UrlPolicy> perUrlPolicyMap = new HashMap<>(); // 每个url限流策略
 	
 
 	public Policy getIpPolicy() {
@@ -29,15 +34,25 @@ public class RateLimitConfig {
 	public void setIpPolicy(Policy ipPolicy) {
 		this.ipPolicy = ipPolicy;
 	}
-
-	public List<IPPolicy> getPerIpPolicies() {
-		return perIpPolicies;
+	
+	public Map<String, IPPolicy> getPerIpPolicyMap() {
+		return perIpPolicyMap;
 	}
-
+	
 	public void setPerIpPolicies(List<IPPolicy> perIpPolicies) {
-		this.perIpPolicies = perIpPolicies;
+		if (perIpPolicies != null) {
+			perIpPolicies.forEach(policy -> {
+				Stream.of(policy.getIp().split(",")).forEach(ip -> {
+					IPPolicy each = new IPPolicy();
+					each.setIp(ip);
+					each.setInterval(policy.getInterval());
+					each.setLimit(policy.getLimit());
+					perIpPolicyMap.put(ip, each);
+				});
+			});
+		}
 	}
-
+	
 	public Policy getUrlPolicy() {
 		return urlPolicy;
 	}
@@ -46,12 +61,29 @@ public class RateLimitConfig {
 		this.urlPolicy = urlPolicy;
 	}
 
-	public List<UrlPolicy> getPerUrlPolicies() {
-		return perUrlPolicies;
+	public Map<String, UrlPolicy> getPerUrlPolicyMap() {
+		return perUrlPolicyMap;
 	}
 
 	public void setPerUrlPolicies(List<UrlPolicy> perUrlPolicies) {
-		this.perUrlPolicies = perUrlPolicies;
+		if (perUrlPolicies != null) {
+			perUrlPolicies.forEach(policy -> {
+				Stream.of(policy.getUrl().split(",")).forEach(url -> {
+					UrlPolicy each = new UrlPolicy();
+					each.setUrl(url);
+					each.setInterval(policy.getInterval());
+					each.setLimit(policy.getLimit());
+					perUrlPolicyMap.put(url, each);
+				});
+			});
+		}
 	}
-	
+
+	public Policy getDefaultPolicy() {
+		return defaultPolicy;
+	}
+
+	public void setDefaultPolicy(Policy defaultPolicy) {
+		this.defaultPolicy = defaultPolicy;
+	}
 }
